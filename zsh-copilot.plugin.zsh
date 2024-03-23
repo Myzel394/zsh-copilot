968a6680cc7f02952901f73ec775151a5076167b
Myzel394 <50424412+Myzel394@users.noreply.github.com>
2024-03-23 22:15:19 +0100

    feat: Add optional context info

--- zsh-copilot.plugin.zsh
+++ zsh-copilot.plugin.zsh
@@ -1,10 +1,20 @@
 (( ! ${+ZSH_COPILOT_KEY} )) &&
-typeset -g ZSH_COPILOT_KEY='^z'
+    typeset -g ZSH_COPILOT_KEY='^z'
+
+(( ! ${+ZSH_COPILOT_SEND_CONTEXT} )) &&
+    typeset -g ZSH_COPILOT_SEND_CONTEXT=true
+
+SYSTEM_PROMPT="You will be given the raw input of a shell command. Your task is to either complete the command or provide a new command that you think the user is trying to type. If you return a completely new command for the user, prefix is with an equal sign (=). If you return a completion for the user's command, prefix it with a plus sign (+). Only respond with either a completion or a new command, not both. Do not explain the command. Do not ask for more information, you won't receive it. Your response will be run in the user's shell. Make sure input is escaped correctly if needed so. Your input should be able to run without any modifications to it. Don't you dare to return anything else other than a shell command!!! DO NOT INTERACT WITH THE USER IN NATURAL LANGUAGE! If you do, you will be banned from the system. Here are two examples: * User input: 'list files in current directory'; Your response: '=ls' * User input: 'cd /tm'; Your response: '+p'."
+
+if [[ "$ZSH_COPILOT_SEND_CONTEXT" == 'true' ]]; then
+    SYSTEM_PROMPT="$SYSTEM_PROMPT Context: You are user ${$(whoami)} with id ${$(id)} in directory ${$(pwd)}. Your shell is ${$(echo $SHELL)} and your terminal is ${$(echo $TERM)} running on ${$(uname -a)}. Your system is ${$(cat /etc/*-release | xargs | sed 's/ /,/g')}."
+fi
 
 function _suggest_ai() {
     # Get input
     local input="${BUFFER:0:$CURSOR}"
 
+    _zsh_autosuggest_clear
     zle -R "Thinking..."
 
     local response=$(curl 'https://api.openai.com/v1/chat/completions' \
@@ -16,7 +26,7 @@ function _suggest_ai() {
             \"messages\": [
                 {
                     \"role\": \"system\",
-                    \"content\": \"You will be given the raw input of a shell command. Your task is to either complete the command or provide a new command that you think the user is trying to type. If you return a completely new command for the user, prefix is with an equal sign (=). If you return a completion for the user's command, prefix it with a plus sign (+). Only respond with either a completion or a new command, not both. Do not explain the command. Do not ask for more information, you won't receive it. Your response will be run in the user's shell. So don't do you dare to return anything else other than a shell command. If you do, you will be banned from the system. Here are two examples: * User input: 'list files in current directory'; Your response: '=ls' * User input: 'cd /tm'; Your response: '+p'\"
+                    \"content\": \"${SYSTEM_PROMPT}\"
                 },
                 {
                     \"role\": \"user\",
@@ -31,11 +41,12 @@ function _suggest_ai() {
     local suggestion=${response:1:${#response}}
 
     if [[ "$first_char" == '=' ]]; then
-        BUFFER="$suggestion"
-        CURSOR=${#BUFFER}
+        BUFFER=""
+        CURSOR=0
+
+        zle -U "$suggestion"
 
         # Reset suggestion
-        _zsh_autosuggest_suggest ''
     elif [[ "$first_char" == '+' ]]; then
         _zsh_autosuggest_suggest "$suggestion"
     fi
